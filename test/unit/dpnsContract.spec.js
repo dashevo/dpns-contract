c0197de72c01916ccea66055d155d9986ff2a587
Djavid Gabibiyan <djavidoff@gmail.com>
2019-07-26 19:53:12 +0500

    Finalized unit tests for DPNS contract

--- test/unit/dpnsContract.spec.js
+++ test/unit/dpnsContract.spec.js
@@ -27,28 +27,13 @@ describe('DPNS Contract', () => {
 
       beforeEach(() => {
         preorderData = {
-          saltedDomainHash: '',
+          saltedDomainHash: Buffer.alloc(32).toString('hex'),
         };
       });
 
       it('should throw validation error if `saltedDomainHash` is not specified', () => {
         delete preorderData.saltedDomainHash;
 
-        const preorder = dpp.document.create('preorder', preorderData);
-
-        const result = dpp.document.validate(preorder);
-
-        expect(result.isValid()).to.be.false();
-        expect(result.errors).to.have.a.lengthOf(1);
-
-        const [error] = result.errors;
-
-        expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('saltedDomainHash');
-      });
-
-      it('should throw validation error if `saltedDomainHash` is invalid', () => {
         const preorder = dpp.document.create('preorder', preorderData);
 
         const result = dpp.document.validate(preorder);
@@ -77,36 +62,19 @@ describe('DPNS Contract', () => {
 
       beforeEach(() => {
         domainData = {
-          hash: '',
-          label: '',
-          normalizedLabel: '',
-          parentDomainHash: '',
-          preorderSalt: '',
-          records: [
-            { dashIdentity: '' },
-          ],
+          hash: Buffer.alloc(32).toString('hex'),
+          label: 'Wallet',
+          normalizedLabel: 'wallet',
+          parentDomainHash: Buffer.alloc(32, 1).toString('hex'),
+          preorderSalt: Buffer.alloc(32, 2).toString('hex'),
+          records: {
+            dashIdentity: Buffer.alloc(32).toString('hex'),
+          },
         };
       });
 
       it('should throw validation error if `hash` is not specified', () => {
         delete domainData.hash;
-
-        const domain = dpp.document.create('domain', domainData);
-
-        const result = dpp.document.validate(domain);
-
-        expect(result.isValid()).to.be.false();
-        expect(result.errors).to.have.a.lengthOf(1);
-
-        const [error] = result.errors;
-
-        expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('hash');
-      });
-
-      it('should throw validation error if `hash` is invalid', () => {
-        domainData.hash = 'invalid hash';
 
         const domain = dpp.document.create('domain', domainData);
 
@@ -152,8 +120,8 @@ describe('DPNS Contract', () => {
         const [error] = result.errors;
 
         expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('label');
+        expect(error.keyword).to.equal('pattern');
+        expect(error.dataPath).to.equal('.label');
       });
 
       it('should throw validation error if `normalizedLabel` is not specified', () => {
@@ -186,8 +154,8 @@ describe('DPNS Contract', () => {
         const [error] = result.errors;
 
         expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('normalizedLabel');
+        expect(error.keyword).to.equal('pattern');
+        expect(error.dataPath).to.equal('.normalizedLabel');
       });
 
       it('should throw validation error if `parentDomainHash` is not specified', () => {
@@ -207,42 +175,8 @@ describe('DPNS Contract', () => {
         expect(error.params.missingProperty).to.equal('parentDomainHash');
       });
 
-      it('should throw validation error if `parentDomainHash` is invalid', () => {
-        domainData.parentDomainHash = 'invalid parent hash';
-
-        const domain = dpp.document.create('domain', domainData);
-
-        const result = dpp.document.validate(domain);
-
-        expect(result.isValid()).to.be.false();
-        expect(result.errors).to.have.a.lengthOf(1);
-
-        const [error] = result.errors;
-
-        expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('parentDomainHash');
-      });
-
       it('should throw validation error if `preorderSalt` is not specified', () => {
         delete domainData.preorderSalt;
-
-        const domain = dpp.document.create('domain', domainData);
-
-        const result = dpp.document.validate(domain);
-
-        expect(result.isValid()).to.be.false();
-        expect(result.errors).to.have.a.lengthOf(1);
-
-        const [error] = result.errors;
-
-        expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('preorderSalt');
-      });
-
-      it('should throw validation error if `preorderSalt` is invalid', () => {
-        domainData.preorderSalt = 'invalid preorder salt';
 
         const domain = dpp.document.create('domain', domainData);
 
@@ -276,7 +210,7 @@ describe('DPNS Contract', () => {
       });
 
       it('should throw validation error if `records` is empty', () => {
-        domainData.records = [];
+        domainData.records = {};
 
         const domain = dpp.document.create('domain', domainData);
 
@@ -288,14 +222,14 @@ describe('DPNS Contract', () => {
         const [error] = result.errors;
 
         expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('normalizedLabel');
+        expect(error.keyword).to.equal('minProperties');
+        expect(error.dataPath).to.equal('.records');
       });
 
-      it('should throw validation error if `records` have an invalid record', () => {
-        domainData.records = [
-          // TODO: add invalid record
-        ];
+      it('should throw validation error if `records` have a short `dashIdentity`', () => {
+        domainData.records = {
+          dashIdentity: 'short indentity',
+        };
 
         const domain = dpp.document.create('domain', domainData);
 
@@ -307,10 +241,28 @@ describe('DPNS Contract', () => {
         const [error] = result.errors;
 
         expect(error.name).to.equal('JsonSchemaError');
-        expect(error.keyword).to.equal('required');
-        expect(error.params.missingProperty).to.equal('normalizedLabel');
+        expect(error.keyword).to.equal('minLength');
+        expect(error.dataPath).to.equal('.records.dashIdentity');
       });
 
+      it('should throw validation error if `records` have a long `dashIdentity`', () => {
+        domainData.records = {
+          dashIdentity: Buffer.alloc(64).toString('hex'),
+        };
+
+        const domain = dpp.document.create('domain', domainData);
+
+        const result = dpp.document.validate(domain);
+
+        expect(result.isValid()).to.be.false();
+        expect(result.errors).to.have.a.lengthOf(1);
+
+        const [error] = result.errors;
+
+        expect(error.name).to.equal('JsonSchemaError');
+        expect(error.keyword).to.equal('maxLength');
+        expect(error.dataPath).to.equal('.records.dashIdentity');
+      });
       it('shoud successfuly validate a domain document is it is valid', () => {
         const domain = dpp.document.create('domain', domainData);
 
@@ -319,9 +271,5 @@ describe('DPNS Contract', () => {
         expect(result.isValid()).to.be.true();
       });
     });
-  });
-
-  describe('data triggers', () => {
-
   });
 });
