63b23152ab35481020d45f4f17d8100c28820fd0
Djavid Gabibyian <djavidoff@gmail.com>
2020-03-17 12:57:18 +0500

    chore: simplify `fetch` script

--- src/fetch.js
+++ src/fetch.js
@@ -1,24 +1,46 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies, no-console */
 const DAPIClient = require('@dashevo/dapi-client');
+
+const DashPlatformProtocol = require('@dashevo/dpp');
+
+const { argv } = require('yargs')
+  .usage(
+    'Usage: $0 --dapiAddress [string] --contractId [string]',
+  )
+  .demandOption(['dapiAddress', 'contractId']);
 
 /**
  * Fetch DPNS contract
  *
- * @param {string} contractId
- *
- * @returns {Promise<Buffer>}
+ * @returns {Promise<void>}
  */
-async function fetch(contractId) {
-  const seeds = process.env.DAPI_CLIENT_SEEDS
-    .split(',')
-    .map((ip) => ({ service: `${ip}:${process.env.DAPI_CLIENT_PORT}` }));
+async function fetch() {
+  const seeds = [
+    { service: argv.dapiAdress },
+  ];
 
   const dapiClient = new DAPIClient({
     seeds,
     timeout: 30000,
   });
 
-  return dapiClient.getContract(contractId);
+  const dpp = new DashPlatformProtocol({ dataProvider: {} });
+
+  const buffer = await dapiClient.getDataContract(argv.contractId);
+
+  if (!buffer || buffer.length === 0) {
+    console.error('Have not been able to found data contract with an id: ', argv.contractId);
+    process.exit(1);
+  }
+
+  const rawDataContract = await dpp.dataContract.createFromSerialized(
+    buffer, { skipValidation: true },
+  );
+
+  console.log(
+    'Here is the data contract JSON: \n',
+    JSON.stringify(rawDataContract, undefined, 2),
+  );
 }
 
 module.exports = fetch;
